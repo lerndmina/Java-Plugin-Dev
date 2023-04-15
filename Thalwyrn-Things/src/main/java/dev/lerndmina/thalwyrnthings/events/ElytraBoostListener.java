7c426e0d7612041471f994856a9a88745fc3028f
Adam <wild@lerndmina.dev>
2023-04-16 00:42:15 +0100

    Fixed issue with elytraboost

--- Thalwyrn-Things/src/main/java/dev/lerndmina/thalwyrnthings/events/ElytraBoostListener.java
+++ Thalwyrn-Things/src/main/java/dev/lerndmina/thalwyrnthings/events/ElytraBoostListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 import static dev.lerndmina.thalwyrnthings.Utils.StringHelpers.*;
@@ -39,10 +40,16 @@ public class ElytraBoostListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     private void onMoveWhileGliding(PlayerMoveEvent event){
+        // If player is not in the list we return as quickly as possible to save resources
         if (!Main.getInstance().currentlyGlideBoosting.contains(event.getPlayer().getUniqueId())) {
             return;
         }
         Player p = event.getPlayer();
+        // If player is not gliding we remove them from the currently boosting list to prevent the boost activating next time they fly.
+        if (!p.isGliding()){
+            Main.getInstance().currentlyGlideBoosting.remove(p.getUniqueId());
+            return;
+        }
         if (p.hasPermission(permission) && p.isGliding()){
             p.setVelocity(p.getEyeLocation().getDirection().normalize().multiply(2));
         }
@@ -61,5 +68,12 @@ public class ElytraBoostListener implements Listener {
         {
             event.setCancelled(true);
         }
+    }
+
+    // Clean up when player leaves server
+    @EventHandler(ignoreCancelled = true)
+    private void onPlayerLeave(PlayerQuitEvent event){
+        // .remove only removes if the object is present, so no need to check if it is in the list.
+        Main.getInstance().currentlyGlideBoosting.remove(event.getPlayer().getUniqueId());
     }
 }
