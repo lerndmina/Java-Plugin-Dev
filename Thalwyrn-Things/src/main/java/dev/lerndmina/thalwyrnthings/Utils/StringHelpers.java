48464d475c23bf2d3fb0dfc56321285abf80e977
Adam <wild@lerndmina.dev>
2023-04-16 00:43:04 +0100

    Changed colour formatting

--- Thalwyrn-Things/src/main/java/dev/lerndmina/thalwyrnthings/Utils/StringHelpers.java
+++ Thalwyrn-Things/src/main/java/dev/lerndmina/thalwyrnthings/Utils/StringHelpers.java
@@ -1,73 +1,49 @@
 package dev.lerndmina.thalwyrnthings.Utils;
 
+import com.iridium.iridiumcolorapi.IridiumColorAPI;
 import dev.lerndmina.thalwyrnthings.Main;
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.bukkit.ChatColor.COLOR_CHAR;
-
 public class StringHelpers {
 
     public static String parseString(String input){
-        String output = parseColor(input);
 
-        return output;
+        return IridiumColorAPI.process(input);
     }
 
     public static String parseString(String input, Player player){
-        String output = parseColor(input);
-        output = PlaceholderAPI.setPlaceholders(player, output);
-
-
-        return output;
+       input = PlaceholderAPI.setPlaceholders(player, input);
+        input = IridiumColorAPI.process(input);
+        return input;
     }
 
-    public static String parseColor(String content) {
-        return (ChatColor.translateAlternateColorCodes('&', translateHexColorCodes("&#","", content)));
-    }
 
 
     public static void playerMsg(Player player, String message) {
-        player.sendMessage(StringHelpers.parseColor(Main.getInstance().getConfig().getString("prefix") + "&f " + message));
+        player.sendMessage(StringHelpers.parseString(Main.getInstance().getConfig().getString("prefix") + "&f " + message, player));
     }
     public void playerClean(Player player, String message) {
-        player.sendMessage(StringHelpers.parseColor(message));
+        player.sendMessage(StringHelpers.parseString(message));
     }
 
     public static void consoleMsg(String message, Main.consoleTypes type){
         if(type == Main.consoleTypes.INFO){
-            Main.getInstance().getLogger().info(StringHelpers.parseColor(message));
+            Main.getInstance().getLogger().info(StringHelpers.parseString(message));
         } else if (type == Main.consoleTypes.WARN) {
-            Main.getInstance().getLogger().warning(StringHelpers.parseColor(message));
+            Main.getInstance().getLogger().warning(StringHelpers.parseString(message));
         } else if (type == Main.consoleTypes.SEVERE) {
-            Main.getInstance().getLogger().severe(StringHelpers.parseColor(message));
+            Main.getInstance().getLogger().severe(StringHelpers.parseString(message));
         }
-    }
-
-    public static String translateHexColorCodes(String startTag, String endTag, String message)
-    {
-        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
-        Matcher matcher = hexPattern.matcher(message);
-        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
-        while (matcher.find())
-        {
-            String group = matcher.group(1);
-            matcher.appendReplacement(buffer, COLOR_CHAR + "&"
-                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
-                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
-                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
-            );
-        }
-        return matcher.appendTail(buffer).toString();
     }
 
     public static void debugPlayerMsg(Player p, String message) {
         if (Main.getInstance().debug) {
-            p.sendMessage(StringHelpers.parseColor("&c&lDEBUG &4&l>> &c" + message));
+            p.sendMessage(StringHelpers.parseString("&c&lDEBUG &4&l>> &c" + message));
         }
     }
 
