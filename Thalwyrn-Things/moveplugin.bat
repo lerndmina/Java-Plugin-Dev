9938a2101b171485f20061e2d9329a424522f1e5
Adam <wild@lerndmina.dev>
2023-05-15 21:20:56 +0100

    Removed thalwyrnthings to own repo

--- Thalwyrn-Things/moveplugin.bat
+++
@@ -1,5 +0,0 @@
-@REM Delete the old file
-del "C:\Users\Wild\Minecraft Testing Server\plugins\thalwyrnThings*.jar"
-
-@REM Move the file thalwyrnThings* to C:\Users\Wild\Minecraft Testing Server\plugins and overwrite the existing file
-move /y ".\target\thalwyrnThings*.jar" "C:\Users\Wild\Minecraft Testing Server\plugins\"
